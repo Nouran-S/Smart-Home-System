4dc7c55fdc2b57424b3068d9568a2005c1ddc82c
Nouran <126247436+Nouran-S@users.noreply.github.com>
2023-09-21 00:14:19 +0300

    Add files via upload

---
+++ U2Code/GIE_interface.h
@@ -0,0 +1,14 @@
+/*
+ * GIE_interface.h
+ *
+ *  Created on: Aug 23, 2023
+ *      Author: PC
+ */
+
+#ifndef GIE_INTERFACE_H_
+#define GIE_INTERFACE_H_
+
+void GIE_vidEnable();
+void GIE_vidDisable();
+
+#endif /* GIE_INTERFACE_H_ */
---
+++ U2Code/GIE_private.h
@@ -0,0 +1,13 @@
+/*
+ * GIE_private.h
+ *
+ *  Created on: Aug 23, 2023
+ *      Author: PC
+ */
+
+#ifndef GIE_PRIVATE_H_
+#define GIE_PRIVATE_H_
+
+#define SREG *((volatile u8*)0x5f)
+
+#endif /* GIE_PRIVATE_H_ */
